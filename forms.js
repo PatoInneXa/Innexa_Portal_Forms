5e582e483c07489c607fe68a8ce034b6d7232a85
PatinhoSV <psoto@innexa.cl>
2026-07-21 17:24:34 -0400

    Add files via upload

---
+++ forms.js
@@ -0,0 +1,107 @@
+const forms = [
+
+  // PREVENCIÓN DE RIESGOS
+
+  {
+    categoria: "Prevención de Riesgos",
+    nombre: "AST Cableado de Seguridad",
+    descripcion: "Formulario de análisis de seguridad para trabajos con cableado.",
+    url: "https://forms.cloud.microsoft/r/qk1SeuAXBW"
+  },
+
+  {
+    categoria: "Prevención de Riesgos",
+    nombre: "AST Extensiones Eléctricas",
+    descripcion: "Formulario de análisis de seguridad para trabajos con extensiones eléctricas",
+    url: "https://forms.cloud.microsoft/r/CxtjjDrbk5"
+  },
+
+  {
+    categoria: "Prevención de Riesgos",
+    nombre: "AST Tablero Eléctrico",
+    descripcion: "Formulario de análisis de seguridad para trabajos con tableros eléctricos.",
+    url: "https://forms.cloud.microsoft/r/sUm58BsgLX"
+  },
+
+  {
+    categoria: "Prevención de Riesgos",
+    nombre: "AST Generador Eléctrico",
+    descripcion: "Formulario de análisis de seguridad para trabajos con generadores eléctricos.",
+    url: "https://forms.cloud.microsoft/r/H6W2R3W6CX"
+  },
+
+  {
+    categoria: "Prevención de Riesgos",
+    nombre: "AST Cambio de Pasadores",
+    descripcion: "Formulario de análisis de seguridad para trabajos con cambio de pasadores.",
+    url: "https://forms.cloud.microsoft/r/TbGtzhVDmh"
+  },
+
+  {
+    categoria: "Prevención de Riesgos",
+    nombre: "AST Trabajos en Caliente",
+    descripcion: "Formulario de análisis de seguridad para trabajos en caliente.",
+    url: "https://forms.cloud.microsoft/r/kjJ6HSPh1i"
+  },
+
+ {
+    categoria: "Prevención de Riesgos",
+    nombre: "AST Flotadores",
+    descripcion: "Formulario de análisis de seguridad para trabajos con flotadores.",
+    url: "https://forms.cloud.microsoft/r/3eSbLFbnKb"
+  },
+
+  {
+    categoria: "Prevención de Riesgos",
+    nombre: "AST Abrazaderas o Corchetes",
+    descripcion: "Formulario de análisis de seguridad para trabajos con abrazaderas o corchetes.",
+    url: "https://forms.cloud.microsoft/r/YBaEmwc0ML"
+  },
+
+  {
+    categoria: "Prevención de Riesgos",
+    nombre: "AST Izaje de Carga",
+    descripcion: "Formulario de análisis de seguridad para trabajos con izaje de carga.",
+    url: "https://forms.cloud.microsoft/r/ync6dej0uF+"
+  },
+
+  {
+    categoria: "Prevención de Riesgos",
+    nombre: "AST Miscelaneos",
+    descripcion: "Formulario de análisis de seguridad para trabajos miscelaneos.",
+    url: "https://forms.cloud.microsoft/r/X1NmVVMUFi"
+  },
+  // RRHH
+
+  {
+    categoria: "RRHH",
+    nombre: "Solicitud de Vacaciones",
+    descripcion: "Gestión de vacaciones.",
+    url: "#"
+  },
+
+
+  // FAENAS
+
+  {
+    categoria: "Faenas",
+    nombre: "Informe Diario de Faenas",
+    descripcion: "Registro de actvidades realizadas en faenas.",
+    url: "https://forms.cloud.microsoft/r/M1vZti6RNs"
+  },
+
+  {
+    categoria: "Faenas",
+    nombre: "Levantamiento de Centro Cliente",
+    descripcion: "Registro de levantamiento de centro cliente.",
+    url: "#"
+  },
+
+  {
+    categoria: "Faenas",
+    nombre: "Cierre de OT",
+    descripcion: "Registro de cierre de Orden de Trabajo (OT)",
+    url: "#"
+  }
+
+];
