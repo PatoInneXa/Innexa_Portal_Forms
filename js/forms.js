bd4a376f3467df4aaaaf7372f1ea18174f380aa3
PatinhoSV <psoto@innexa.cl>
2026-07-23 11:40:22 -0400

    Add files via upload

--- js/forms.js
+++ js/forms.js
@@ -10,95 +10,95 @@ const forms = [
   },
 
   {
-    categoria: "Prevención de Riesgos",
+    categoria: "Prevención de Riesgos - Rio Yelcho",
     nombre: "AST Extensiones Eléctricas",
     descripcion: "Formulario de análisis de seguridad para trabajos con extensiones eléctricas",
     url: "https://forms.cloud.microsoft/r/CxtjjDrbk5"
   },
 
   {
-    categoria: "Prevención de Riesgos",
+    categoria: "Prevención de Riesgos - Rio Yelcho",
     nombre: "AST Tablero Eléctrico",
     descripcion: "Formulario de análisis de seguridad para trabajos con tableros eléctricos.",
     url: "https://forms.cloud.microsoft/r/sUm58BsgLX"
   },
 
   {
-    categoria: "Prevención de Riesgos",
+    categoria: "Prevención de Riesgos - Rio Yelcho",
     nombre: "AST Generador Eléctrico",
     descripcion: "Formulario de análisis de seguridad para trabajos con generadores eléctricos.",
     url: "https://forms.cloud.microsoft/r/H6W2R3W6CX"
   },
 
   {
-    categoria: "Prevención de Riesgos",
+    categoria: "Prevención de Riesgos - Rio Yelcho",
     nombre: "AST Cambio de Pasadores",
     descripcion: "Formulario de análisis de seguridad para trabajos con cambio de pasadores.",
     url: "https://forms.cloud.microsoft/r/TbGtzhVDmh"
   },
 
   {
-    categoria: "Prevención de Riesgos",
+    categoria: "Prevención de Riesgos - Rio Yelcho",
     nombre: "AST Trabajos en Caliente",
     descripcion: "Formulario de análisis de seguridad para trabajos en caliente.",
     url: "https://forms.cloud.microsoft/r/kjJ6HSPh1i"
   },
 
  {
-    categoria: "Prevención de Riesgos",
+    categoria: "Prevención de Riesgos - Rio Yelcho",
     nombre: "AST Flotadores",
     descripcion: "Formulario de análisis de seguridad para trabajos con flotadores.",
     url: "https://forms.cloud.microsoft/r/3eSbLFbnKb"
   },
 
   {
-    categoria: "Prevención de Riesgos",
+    categoria: "Prevención de Riesgos - Rio Yelcho",
     nombre: "AST Abrazaderas o Corchetes",
     descripcion: "Formulario de análisis de seguridad para trabajos con abrazaderas o corchetes.",
     url: "https://forms.cloud.microsoft/r/YBaEmwc0ML"
   },
 
   {
-    categoria: "Prevención de Riesgos",
+    categoria: "Prevención de Riesgos - Rio Yelcho",
     nombre: "AST Izaje de Carga",
     descripcion: "Formulario de análisis de seguridad para trabajos con izaje de carga.",
     url: "https://forms.cloud.microsoft/r/ync6dej0uF+"
   },
 
   {
-    categoria: "Prevención de Riesgos",
+    categoria: "Prevención de Riesgos - Rio Yelcho",
     nombre: "AST Miscelaneos",
     descripcion: "Formulario de análisis de seguridad para trabajos miscelaneos.",
     url: "https://forms.cloud.microsoft/r/X1NmVVMUFi"
   },
-  // RRHH
+  // RIO AYSEN
 
   {
-    categoria: "RRHH",
-    nombre: "Solicitud de Vacaciones",
-    descripcion: "Gestión de vacaciones.",
-    url: "#"
+    categoria: "Prevención de Riesgos - Rio Aysén",
+    nombre: "AST - Rio Aysen",
+    descripcion: "Formulario de analisis de seguridad para trabajos de Rio Aysen.",
+    url: "https://forms.cloud.microsoft/r/a9TfNUdk6H"
   },
 
 
   // FAENAS
 
   {
-    categoria: "Faenas",
+    categoria: "Faenas - Rio Yelcho",
     nombre: "Informe Diario de Faenas",
     descripcion: "Registro de actvidades realizadas en faenas.",
-    url: "#"
+    url: "https://forms.cloud.microsoft/r/M1vZti6RNs"
   },
 
   {
-    categoria: "Faenas",
+    categoria: "Faenas - Rio Yelcho",
     nombre: "Levantamiento de Centro Cliente",
     descripcion: "Registro de levantamiento de centro cliente.",
     url: "#"
   },
 
   {
-    categoria: "Faenas",
+    categoria: "Faenas - Rio Yelcho",
     nombre: "Cierre de OT",
     descripcion: "Registro de cierre de Orden de Trabajo (OT)",
     url: "#"
